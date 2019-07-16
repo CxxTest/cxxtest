CxxTest/cxxtest

commit 0c764fbe9d9db91fba4d81555e154b4fdbdacfd8

Adding logic to skip tests.
The TS_SKIP() macro supports test skipping.

cxxtest/unix.h
@@ -48,8 +48,12 @@ namespace CxxTest
         {
             if ( tracker().failedTests() ) {
                 (*_o) << "Failed " << tracker().failedTests() << " of " << totalTests << endl;
-                unsigned numPassed = desc.numTotalTests() - tracker().failedTests();
-                (*_o) << "Success rate: " << (numPassed * 100 / desc.numTotalTests()) << "%" << endl;
+                unsigned numPassed = desc.numTotalTests() - tracker().failedTests() - tracer().skippedTests();
+                unsigned numTotal = desc.numTotalTests() - tracker().skippedTests();
+                if (numTotal == 0)
+                    (*_o) << "Success rate: 100%" << endl;
+                else
+                    (*_o) << "Success rate: " << (numPassed * 100.0 / numTotal) << "%" << endl;
             }
         }
 
@@ -62,6 +66,12 @@ namespace CxxTest
         void warning( const char *file, int line, const char *expression )
         {
             stop( file, line ) << "Warning: " <<
+                expression << endl;
+        }
+
+        void skippedTest( const char *file, int line, const char *expression )
+        {
+            stop( file, line ) << "Warning: Test skipped: " <<
                 expression << endl;
         }
 

cxxtest/ErrorFormatter.h
@@ -81,7 +81,12 @@ public:
     }
 
     void leaveTest(const TestDescription &) {
-        if (!tracker().testFailed()) {
+        if (tracker().testSkipped()) {
+            (*_o) << "s";
+            _o->flush();
+            fflush(stdout);
+            _dotting = true;
+        } else if (!tracker().testFailed()) {
             (*_o) << ".";
             _o->flush();
             fflush(stdout);
@@ -95,9 +100,13 @@ public:
             return;
         }
         newLine();
-        (*_o) << "Failed " << tracker().failedTests() << " of " << totalTests << endl;
-        unsigned numPassed = desc.numTotalTests() - tracker().failedTests();
-        (*_o) << "Success rate: " << (numPassed * 100 / desc.numTotalTests()) << "%" << endl;
+        (*_o) << "Failed " << tracker().failedTests() << " and Skipped " << tracker().skippedTests() << " of " << totalTests << endl;
+        unsigned numPassed = desc.numTotalTests() - tracker().failedTests() - tracker().skippedTests();
+        unsigned numTotal = desc.numTotalTests() - tracker().skippedTests();
+        if (numTotal == 0)
+            (*_o) << "Success rate: 100%" << endl;
+        else
+            (*_o) << "Success rate: " << (numPassed * 100.0 / numTotal) << "%" << endl;
     }
 
     void trace(const char *file, int line, const char *expression) {
@@ -107,6 +116,11 @@ public:
 
     void warning(const char *file, int line, const char *expression) {
         stop(file, line) << _warningString << ": " <<
+                         expression << endl;
+    }
+
+    void skippedTest(const char *file, int line, const char *expression) {
+        stop(file, line) << _warningString << ": Test skipped: " <<
                          expression << endl;
     }
 

cxxtest/Gui.h
@@ -57,6 +57,10 @@ public:
         yellowBarSafe();
     }
 
+    void skippedTest(const char * /*file*/, int /*line*/, const char * /*expression*/) {
+        yellowBarSafe();
+    }
+
     void failedTest(const char * /*file*/, int /*line*/, const char * /*expression*/) {
         redBarSafe();
     }

cxxtest/RealDescriptions.cpp
@@ -58,10 +58,11 @@ bool RealTestDescription::setUp() {
 
     _TS_TRY {
         bool ok = false;
-        _TSM_ASSERT_THROWS_NOTHING(file(), line(), "Exception thrown from setUp()", suite()->setUp(); ok = true);
+        _TSM_ASSERT_THROWS_NOTHING(file(), line(), "Exception thrown from setUp()", suite()->setUp(); ok=true);
         if (ok == false) { return ok; }
     }
-    _TS_CATCH_ABORT( { return false; });
+    _TS_CATCH_ABORT( { return false; })
+    _TS_CATCH_SKIPPED( { return false; });
 
     return true;
 }
@@ -74,7 +75,8 @@ bool RealTestDescription::tearDown() {
     _TS_TRY {
         _TSM_ASSERT_THROWS_NOTHING(file(), line(), "Exception thrown from tearDown()", suite()->tearDown());
     }
-    _TS_CATCH_ABORT( { return false; });
+    _TS_CATCH_ABORT( { return false; })
+    _TS_CATCH_SKIPPED( { return false; });
 
     for (GlobalFixture *gf = GlobalFixture::lastGlobalFixture(); gf != 0; gf = gf->prevGlobalFixture()) {
         bool ok;
@@ -103,6 +105,7 @@ TestSuite *RealTestDescription::suite() const { return _suite->suite(); }
 void RealTestDescription::run() {
     _TS_TRY { runTest(); }
     _TS_CATCH_ABORT( {})
+    _TS_CATCH_SKIPPED( {})
     ___TSM_CATCH(file(), line(), "Exception thrown from test");
 }
 

cxxtest/RealDescriptions.h
@@ -179,7 +179,8 @@ bool DynamicSuiteDescription<S>::setUp() {
         _TSM_ASSERT_THROWS_NOTHING(file(), _createLine, "Exception thrown from createSuite()", createSuite());
         _TSM_ASSERT(file(), _createLine, "createSuite() failed", suite() != 0);
     }
-    _TS_CATCH_ABORT( { return false; });
+    _TS_CATCH_ABORT( { return false; })
+    _TS_CATCH_SKIPPED( { return false; });
 
     return (suite() != 0);
 }
@@ -193,7 +194,8 @@ bool DynamicSuiteDescription<S>::tearDown() {
     _TS_TRY {
         _TSM_ASSERT_THROWS_NOTHING(file(), _destroyLine, "destroySuite() failed", destroySuite());
     }
-    _TS_CATCH_ABORT( { return false; });
+    _TS_CATCH_ABORT( { return false; })
+    _TS_CATCH_SKIPPED( { return false; });
 
     return true;
 }

cxxtest/TeeListener.h
@@ -63,6 +63,11 @@ public:
         _second->warning(file, line, expression);
     }
 
+    void skippedTest(const char *file, int line, const char *expression) {
+        _first->skippedTest(file, line, expression);
+        _second->skippedTest(file, line, expression);
+    }
+
     void failedTest(const char *file, int line, const char *expression) {
         _first->failedTest(file, line, expression);
         _second->failedTest(file, line, expression);

cxxtest/TestListener.h
@@ -37,6 +37,8 @@ public:
                        const char * /*expression*/) {}
     virtual void warning(const char * /*file*/, int /*line*/,
                          const char * /*expression*/) {}
+    virtual void skippedTest(const char * /*file*/, int /*line*/,
+                            const char * /*expression*/) {}
     virtual void failedTest(const char * /*file*/, int /*line*/,
                             const char * /*expression*/) {}
     virtual void failedAssert(const char * /*file*/, int /*line*/,

cxxtest/TestSuite.cpp
@@ -72,6 +72,16 @@ void doWarn(const char *file, int line, const char *message) {
     tracker().warning(file, line, message);
 }
 
+#   if defined(_CXXTEST_HAVE_EH)
+void doSkipTest(const char* file, int line, const char* message) {
+    tracker().skippedTest(file, line, message);
+    throw SkipTest();
+#   else
+void doSkipTest(const char* file, int line, const char*) {
+    doWarn(file, line, "Test skipping is not supported without exception handling.");
+#   endif
+}
+
 void doFailTest(const char *file, int line, const char *message) {
     tracker().failedTest(file, line, message);
     TS_ABORT();

commit 67c374d9750d539a936f3fc588a420a8e3156184

Fix typo introduced in 0c764fbe9d9db91fba4d81555e154b4fdbdacfd8

cxxtest/unix.h
@@ -60,7 +60,7 @@ public:
         if (tracker().failedTests())
         {
             (*_o) << "Failed " << tracker().failedTests() << " of " << totalTests << endl;
-            unsigned numPassed = desc.numTotalTests() - tracker().failedTests() - tracer().skippedTests();
+            unsigned numPassed = desc.numTotalTests() - tracker().failedTests() - tracker().skippedTests();
             unsigned numTotal = desc.numTotalTests() - tracker().skippedTests();
             if (numTotal == 0)
             {